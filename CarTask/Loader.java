c7f63dc6d9f1e48a3768b1d140395e5bbc578ee1
mpoklad <102992186+poksik25@users.noreply.github.com>
2022-05-10 16:37:01 +0300

    Update Loader.java

--- CarTask/Loader.java
+++ CarTask/Loader.java
@@ -1,48 +1,43 @@
 package Lesson1.CarTask;
 
 public class Loader {
+    public static final int AMOUNT_OF_CARS = 5; // чтобы не было просто 5, мы объясняем что в массиве.
+
     public static void main(String[] args) {
-        Car car[] = new Car[5];//Массив объектов отличается одной особенностью - они состоят из ссылок на объекты:
+        Car car[] = new Car[AMOUNT_OF_CARS];//Массив объектов отличается одной особенностью - они состоят из ссылок на объекты.
+        car[0] = new Car("Audi", "АН7656ЛД", 1131.444);
+        System.out.println(car[0]);
 
-        car[0] = new Car();
-        car[0].setCarBrand("Porsche");
-        car[0].setNumberOfCar(1433);
-        car[0].setMileage(11333.33);
+        car[1] = new Car("BMW", "ИО3376НО", 232.444);
+        car[2] = new Car("Opel", "АЕ4432ОО", 98743.33);
+        car[3] = new Car("Mercedes", "YY4434LK", 332.3);
+        car[4] = new Car("Porsche", "ВВ4443ЛЛ", 3131.33);
 
-        car[1] = new Car();
-        car[1].setCarBrand("Opel");
-        car[1].setNumberOfCar(1873);
-        car[1].setMileage(563333.33);
+        car[3].rideCar(1000);
+        car[4].rideCar(333);
 
-        car[2] = new Car();
-        car[2].setCarBrand("Kia");
-        car[2].setNumberOfCar(2133);
-        car[2].setMileage(23133.33);
+        for (int i = 0; i < car.length; i++){
+            car[i].rideCar(10000);
+        }
 
-        car[3] = new Car();
-        car[3].setCarBrand("BMW");
-        car[3].setNumberOfCar(7243);
-        car[3].setMileage(43133.33);
+        double maxMileage = car[0].getMileage();
 
-        car[4] = new Car();
-        car[4].setCarBrand("Audi");
-        car[4].setNumberOfCar(1043);
-        car[4].setMileage(69133.33);
-        car[4].rideCar(1000000.0);
-
-        double max = car[0].getMileage();
-        int temp = 0;
+        Car carWithMaxMileage = car[0];
 
         for (int i = 0; i < car.length; i++) {
-            //  double maxCar = car[0].getMileage();
-            if (max < car[i].getMileage()) {
-                max = car[i].getMileage();
-                temp = i;
+            if (maxMileage < car[i].getMileage()) {
+                maxMileage = car[i].getMileage();
+                carWithMaxMileage = car[i];
             }
         }
 
-        System.out.println("Максимальный пробег - " + max + " миль" +
-                ", у машины c номером: " + car[temp].getNumberOfCar());
-    }
-}
+        System.out.println("Max mileage is - " + maxMileage + " mille" +
+                ", at the car with the number: " + carWithMaxMileage.getNumberOfCar());
 
+       for (int i = 0; i < car.length;i++){
+           car[i].getInfo();
+       }
+        System.out.println(car);
+    }
+
+}
